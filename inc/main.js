2aa18475f0a2acddf1d9b9b776f157ec69cca639
Callum Jones <108936019+Devilswrathed@users.noreply.github.com>
2024-04-20 21:46:38 +0100

    Add files via upload

---
+++ inc/main.js
@@ -0,0 +1,5 @@
+document.getElementById("popUp").addEventListener("click", popUp);
+
+function popUp() {
+    alert("Call us on 0191 716 2635");
+}
---
+++ navigation.php
@@ -0,0 +1,29 @@
+<?php
+
+echo '
+
+<ul>
+    <a href="index.html">
+        <li>Home</li>
+    </a>
+
+    <a href="treatment.html">
+        <li>Treatment</li>
+    </a>
+
+    <a href="about.html">
+        <li>About</li>
+    </a>
+
+    <a href="contact.html">
+        <li>Contact</li>
+    </a>
+
+    <a href="reviews.html">
+        <li>Reviews</li>
+    </a>
+</ul>
+
+'
+
+?>
