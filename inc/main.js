42bf3a7e1630d2d5cd0895b4476242ef42bf496c
Callum Jones <108936019+Devilswrathed@users.noreply.github.com>
2024-04-21 21:47:07 +0100

    Update main.js

--- inc/main.js
+++ inc/main.js
@@ -1,5 +1,20 @@
-document.getElementById("popUp").addEventListener("click", popUp);
+let slideIndex = 0;
+showSlides();
 
-function popUp() {
-    alert("Call us on 0191 716 2635");
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < slides.length; i++) {
+    slides[i].className = slides[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
