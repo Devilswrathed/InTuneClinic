bde5dbf9565919b5c8863c946e954c2f8de080a4
Callum Jones <108936019+Devilswrathed@users.noreply.github.com>
2024-05-29 13:46:35 +0000

    added TACs to all navigation

--- navigation.php
+++ navigation.php
@@ -2,27 +2,33 @@
 
 echo '
 
-<ul>
-    <a href="index.html">
-        <li>Home</li>
-    </a>
+<nav>
+        <ul>
+            <a href="index.html">
+                <li>Home</li>
+            </a>
 
-    <a href="treatment.html">
-        <li>Treatment</li>
-    </a>
+            <a href="treatment.html">
+                <li>Treatment</li>
+            </a>
 
-    <a href="about.html">
-        <li>About</li>
-    </a>
+            <a href="about.html">
+                <li>About</li>
+            </a>
 
-    <a href="contact.html">
-        <li>Contact</li>
-    </a>
+            <a href="contact.html">
+                <li>Contact</li>
+            </a>
 
-    <a href="reviews.html">
-        <li>Reviews</li>
-    </a>
-</ul>
+            <a href="reviews.html">
+                <li>Reviews</li>
+            </a>
+
+            <a href="tacs.html">
+              <li>Terms & Conditions</li>
+          </a>
+        </ul>
+    </nav>
 
 '
 
