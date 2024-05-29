786e033a51cd5cdc43b2315ce174612a9a0b48f9
Callum Jones <108936019+Devilswrathed@users.noreply.github.com>
2024-05-29 08:35:58 +0000

    Major responsive updates and added WhatsApp API

---
+++ socials.php
@@ -0,0 +1,30 @@
+<?php
+
+echo '
+
+<section class="socials">
+
+        <div>
+            <a href="https://www.facebook.com/intuneclinic">
+                <img src="images/socials/facebook.png" width="100%" height="100%">
+            </a>
+        </div>
+
+        <div>
+            <a href="https://www.instagram.com/intuneclinic/">
+                <img src="images/socials/instagram.png" width="100%" height="100%">
+            </a>
+        </div>
+
+        <div>
+            <a href="https://wa.me/441917162635?text=Hello%20InTune%20Clinic">
+                <img src="images/socials/whatsapp.png" width="100%" height="100%">
+            </a>
+        </div>
+
+    </section>
+
+'
+
+?>
+
