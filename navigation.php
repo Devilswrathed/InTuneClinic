5b0eba9e9fded4a0e142afa03bf0dec2d79384fb
Callum Jones <108936019+Devilswrathed@users.noreply.github.com>
2024-05-31 09:12:04 +0000

    Updates to social links, with mobile implementation starting.

--- navigation.php
+++ navigation.php
@@ -2,33 +2,7 @@
 
 echo '
 
-<nav>
-        <ul>
-            <a href="index.html">
-                <li>Home</li>
-            </a>
 
-            <a href="treatment.html">
-                <li>Treatment</li>
-            </a>
-
-            <a href="about.html">
-                <li>About</li>
-            </a>
-
-            <a href="contact.html">
-                <li>Contact</li>
-            </a>
-
-            <a href="reviews.html">
-                <li>Reviews</li>
-            </a>
-
-            <a href="tacs.html">
-              <li>Terms & Conditions</li>
-          </a>
-        </ul>
-    </nav>
 
 '
 
