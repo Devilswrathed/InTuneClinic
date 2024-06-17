e1e2edc677f4a2b347223fafee7ee5f30af71a50
Callum Jones <108936019+Devilswrathed@users.noreply.github.com>
2024-06-17 09:14:59 +0000

    Updates from Osteopathic to Osteopathy

--- footer.php
+++ footer.php
@@ -1,54 +1,8 @@
 <?php
 
-echo '     
+echo '
 
-<footer>
-<div class="footer-container">
-    <div class="footer-section">
-        <h3>Contact Us</h3>
-        <p>
-            Address: <a href="https://maps.app.goo.gl/BANt3vcwTXqNfvMm9"> Intune Clinic<br>
-            Hypoint Business Centre, <br>Saltmeadows Rd, <br>Gateshead, <br>NE8 3AH
-                    </a><br>
-            Phone: <a href="tel:+441917162635">0191 716 2635</a><br>
-            Email: <a href="mailto:info@intuneclinic.com">info@intuneclinic.com</a><br>
-            WhatsApp: <a href="https://wa.me/447717063214?text=Hello%20InTune%20Clinic" target="_blank">Chat with us</a>
-        </p>
-    </div>
 
-    <div class="footer-section">
-        <h3>Quick Links</h3>
-        <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="treatment.html">Treatment</a></li>
-            <li><a href="TeamMembers.html">Team Members</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="reviews.html">Reviews</a></li>
-            <li><a href="tacs.html">Terms & Conditions</a></li>
-            <li><a href="QAA.html">Frequent Questions</a></li>
-        </ul>
-    </div>
-
-    <div class="footer-section">
-        <h3>Follow Us</h3>
-        <div class="social-links">
-            <a href="https://www.facebook.com/intuneclinic" target="_blank">
-                <img src="images/socials/facebook.png" alt="Facebook" width="30" height="30">
-            </a>
-            <a href="https://www.instagram.com/intuneclinic/" target="_blank">
-                <img src="images/socials/instagram.png" alt="Instagram" width="30" height="30">
-            </a>
-            <a href="https://wa.me/447717063214?text=Hello%20InTune%20Clinic" target="_blank">
-                <img src="images/socials/whatsapp.png" alt="WhatsApp" width="30" height="30">
-            </a>
-        </div>
-    </div>
-</div>
-
-<div class="footer-bottom">
-    <p>&copy; 2024 InTune Clinic. All Rights Reserved.</p>
-</div>
-</footer>
 
 '
 
